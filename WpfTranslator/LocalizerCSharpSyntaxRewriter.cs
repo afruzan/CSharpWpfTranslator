d443cdfe2b8eb5f9ae49c2fb604339e2c140fd8e
a.foroozannejad <a.foroozannejad@kama.com>
2023-08-16 19:20:30 +0330

    add csharp syntax visitor

---
+++ WpfTranslator/LocalizerCSharpSyntaxRewriter.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTranslator
+{
+    public class LocalizerCSharpSyntaxRewriter : CSharpSyntaxRewriter
+    {
+        [return: NotNullIfNotNull("node")]
+        public override SyntaxNode? Visit(SyntaxNode? node)
+        {
+            switch (node?.Kind())
+            {
+                case SyntaxKind.StringLiteralExpression: // "str" and @"str"
+                    break;
+                case SyntaxKind.InterpolatedStringExpression: // $"a{b}c" and @$"a{b}c"
+                    break;
+                case SyntaxKind.InterpolatedStringText: // "a" and "c" from $"a{b}c"
+                    break;
+            }
+            return base.Visit(node);
+        }
+
+        public override SyntaxToken VisitToken(SyntaxToken token)
+        {
+            switch (token.Kind())
+            {
+                case SyntaxKind.StringLiteralToken: // "str" and @"str"
+                    var text = token.ValueText;
+
+                    text += "#"; // todo
+
+                    return SyntaxFactory.Token(
+                        token.LeadingTrivia,
+                        SyntaxKind.StringLiteralToken,
+                        $"\"{text}\"", $"\"{text}\"",
+                        token.TrailingTrivia);
+
+                case SyntaxKind.InterpolatedStringTextToken: // "a" and "c" from $"a{b}c"
+                    var textPart = token.ValueText;
+
+                    textPart += "#"; // todo
+
+                    return SyntaxFactory.Token(
+                        token.LeadingTrivia,
+                        SyntaxKind.InterpolatedStringTextToken,
+                        textPart, textPart,
+                        token.TrailingTrivia);
+
+                default:
+                    return base.VisitToken(token);
+            }
+        }
+
+    }
+
+}
---
+++ WpfTranslator/TestWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using CSharpTranslator;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTranslator
+{
+    /// <summary>
+    /// Interaction logic for TestWindow.xaml
+    /// </summary>
+    public partial class TestWindow : Window
+    {
+        public TestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var code = @"using Vira;
+
+namespace System.ComponentModel
+{
+    public class LocalizedDescriptionAttribute : DescriptionAttribute
+    {
+        private readonly string _resourceKey;
+        public LocalizedDescriptionAttribute(string resourceKey)
+        {
+            _resourceKey = resourceKey;
+        }
+
+        public override string Description
+        {
+            get
+            {
+                string displayName = LocalizationManager.GetString(""STRING!"") ?? $""ONE{_resourceKey}TWO"";
+
+                return string.IsNullOrEmpty(displayName)
+                    ? string.Format(@""[[{0}]]"", _resourceKey)
+                    : displayName;
+
+                return string.IsNullOrEmpty(displayName)
+                    ? string.Format($@""[[{0}]]"", _resourceKey)
+                    : displayName;
+
+                return string.IsNullOrEmpty(displayName)
+                    ? string.Format(@""[[{0}]]
+Line2
+"", _resourceKey)
+                    : displayName;
+            }
+        }
+    }
+
+}
+";
+
+            var sucess = SyntaxFactory.ParseSyntaxTree(code).TryGetRoot(out var code_parsed);
+            if (sucess)
+            {
+                var newCode = new LocalizerCSharpSyntaxRewriter().Visit(code_parsed)?.ToFullString();
+
+                Debugger.Break();
+            }
+
+            Debugger.Break();
+
+        }
+    }
+}
